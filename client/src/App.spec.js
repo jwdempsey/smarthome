0bc6f35c7a1296feadc488b2785972cfc20449f4
Joe Dempsey <joedempsey@Joes-MacBook-Air.local>
2022-04-13 09:30:57 -0400

    removing a line

--- client/src/App.spec.js
+++ client/src/App.spec.js
@@ -1,6 +1,5 @@
 import { act } from "react-dom/test-utils";
 import App from "./App";
-import { TypeContext } from "./contexts/TypeContext";
 
 describe("App Component", () => {
   it("renders NavigationBar Component", async () => {
